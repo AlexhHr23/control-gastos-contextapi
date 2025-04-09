371c14202df1f2a8fe2cbdca41aa0b4789376127
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-08 22:19:41 -0600

    Validando presupuestos

--- src/components/BudgetForm.tsx
+++ src/components/BudgetForm.tsx
@@ -1,13 +1,18 @@
-import { useState, ChangeEvent } from "react"
+import { useState, ChangeEvent, useMemo } from "react"
 
 export const BudgetForm = () => {
 
-    const [budget, useBudget] = useState(0)
+    const [budget, setBudget] = useState(0)
 
     const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-        console.log(e.target.name);
-        console.log(e.target.id);
+        setBudget(e.target.valueAsNumber)
+        
     }
+
+    const isValid = useMemo(() => {
+        return  isNaN(budget) || budget <= 0
+    }, [budget])
+
     return (
         <form className="space-y-5">
             <div className="flex flex-col space-y-5">
@@ -15,7 +20,7 @@ export const BudgetForm = () => {
                     Definir presupuesto
                 </label>
                 <input
-                    id="budgetID"
+                    id="budget"
                     type="number"
                     className="w-full bg-white border border-gray-200 p-2"
                     placeholder="Define tu presupuesto"
@@ -28,8 +33,10 @@ export const BudgetForm = () => {
             <input
                 type="submit"
                 value="Definir presupeusto"
-                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase"
+                className="bg-blue-600 hover:bg-blue-700 cursor-pointer w-full p-2 text-white font-black uppercase disabled:opacity-10"
+                disabled={isValid}
             />
         </form>
     )
 }
+  
