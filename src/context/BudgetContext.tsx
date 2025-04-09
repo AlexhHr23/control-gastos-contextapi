e5726095c1aa66a9271b9bf6e882c16269118d12
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-08 22:52:45 -0600

    Declarando context

---
+++ src/context/BudgetContext.tsx
@@ -0,0 +1,29 @@
+import { useReducer, createContext, ActionDispatch, ReactNode  } from "react"
+import { initialState, budgetReducer, BudgetState, budgetActions } from "../reducers/budget-reducer"
+
+type BudgetContextProps = {
+    state: BudgetState
+    dispatch: ActionDispatch<[action: budgetActions]>
+}
+
+type BudgetProviderProps = {
+    children: ReactNode
+}
+
+
+export const BudgetContext = createContext<BudgetContextProps>(null!)
+
+export const BudgetProvider =  ({children}: BudgetProviderProps) =>{
+
+
+    const [state, dispatch] = useReducer(budgetReducer, initialState)
+ 
+    return (
+        <BudgetContext.Provider 
+        value={{
+            state, dispatch
+        }}>
+            {children}
+        </BudgetContext.Provider>
+    )
+}
