da0f9f288519788e7b708b82c3dc8db9d59a040a
ALexhHr23 <alex.hernandez24kb@gmail.com>
2025-04-09 22:39:11 -0600

    Componente de cantidades

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 
+import { useMemo } from "react"
 import { BudgetForm } from "./components/BudgetForm"
+import { useBudget } from "./hook/useBudget"
+import BudgetTracker from "./components/BudgetTracker"
+
 
 function App() {
+
+  const {state } = useBudget()
+
+  const isValidBudget = useMemo(() => state.budget > 0 ,[state.budget])
 
   return ( 
     <>
@@ -11,7 +19,7 @@ function App() {
         </h1>
       </header>
       <div className="max-w-3xl mx-auto bg-white shadow-lg rounded-lg mt-10 p-10">
-        <BudgetForm/>
+        {isValidBudget ? <BudgetTracker/> :  <BudgetForm/>  }
       </div>
     </>
   )
